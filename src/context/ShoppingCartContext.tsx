91d4a5b1dd604fe256192ff1ff07be09e124956e
rock <rock891015125673@gmail.com>
2023-03-05 15:32:42 +0800

    Add Open/Close Cart functions and adjust deafult number of navbar Cart button

--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -10,10 +10,14 @@ type CartItem = {
 }
 
 type ShoppingCartContext = {
+    openCart: () => void
+    closeCart: () => void
     getItemQuantity: (id: number) => number
     increaseCartQuantity: (id: number) => void
     decreaseCartQuantity: (id: number) => void
     removeFromCart: (id: number) => void
+    cartQuantity: number
+    cartItems: CartItem[] 
 }
 
 const ShoppingCartContext = createContext({} as ShoppingCartContext)
@@ -23,12 +27,17 @@ export function useShoppingCart () {
 }
 
 export function ShoppingCartProvider ({ children }: ShoppingCartProviderProps) {
+    const [isOpen, setIsOpen] = useState(false)
     const [cartItems, setCartItems] = useState<CartItem[]>([])
 
+    const cartQuantity = cartItems.reduce(
+        ( quantity, item) => item.quantity + quantity, 0)
+
+    const openCart = () => setIsOpen (true)
+    const closeCart = () => setIsOpen (false)
     function getItemQuantity (id: number) {
         return cartItems.find(item => item.id === id)?.quantity || 0
     }
-
     function increaseCartQuantity(id: number) {
         setCartItems((currItems) => {
           const existingItem = currItems.find((item) => item.id === id);
@@ -40,9 +49,7 @@ export function ShoppingCartProvider ({ children }: ShoppingCartProviderProps) {
             );
           }
         });
-      }
-      
-
+    }
       function decreaseCartQuantity(id: number) {
         setCartItems((currItems) => {
           const existingItem = currItems.find((item) => item.id === id);
@@ -54,8 +61,7 @@ export function ShoppingCartProvider ({ children }: ShoppingCartProviderProps) {
             );
           }
         });
-      }
-
+    }
     function removeFromCart (id: number) {
         setCartItems(currItems => {
             return currItems.filter(item => item.id !== id)
@@ -68,7 +74,11 @@ export function ShoppingCartProvider ({ children }: ShoppingCartProviderProps) {
             getItemQuantity,
             increaseCartQuantity,
             decreaseCartQuantity,
-            removeFromCart
+            removeFromCart,
+            openCart,
+            closeCart,
+            cartItems,
+            cartQuantity,
             }}
         >
             {children}
